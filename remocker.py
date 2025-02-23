45573947f22a63d6fc0547db5f45505e3305b8e5
Jrog <jrog612@gmail.com>
2025-02-24 02:47:51 +0900

    change dependency from httpretty to responses

--- remocker.py
+++ remocker.py
@@ -3,7 +3,7 @@ import re
 from contextlib import contextmanager
 from functools import cached_property
 
-import httpretty
+import responses
 
 
 def join_path(str1, str2):
@@ -20,7 +20,7 @@ def join_path(str1, str2):
 
 
 class Remocker:
-    def __init__(self, base_url, ):
+    def __init__(self, base_url):
         self.storages = []
         self.mockers = []
         if callable(base_url):
@@ -28,76 +28,57 @@ class Remocker:
         else:
             self.base_url = base_url
 
-    def get_full_uri(self, uri):
-        if not uri.startswith('http'):
-            return join_path(self.base_url, uri)
-        return uri
+    def get_full_url(self, url):
+        if not url.startswith('http'):
+            return join_path(self.base_url, url)
+        return url
 
-    def get_uri_params(self, uri, pattern):
-        if type(pattern) in (str, bytes):
-            pattern = re.compile(pattern)
-        return re.search(pattern, uri)
-
-    def _append_mocker(self, method, uri, body, regex=False, **kwargs):
-        if not callable(body):
-            body = _json.dumps(body)
-
-        if type(uri) is str:
-            uri = self.get_full_uri(uri)
+    def _append_mocker(self, method, url, callback, regex=False, **kwargs):
+        if type(url) is str:
+            url = self.get_full_url(url)
 
         if regex:
-            uri = re.compile(uri)
+            url = re.compile(url)
 
         self.mockers.append({
             'method': method.upper(),
-            'uri': uri,
-            'body': body,
-            'regex': regex,
+            'url': url,
+            'callback': callback,
             **kwargs,
         })
 
-    def mock(self, method, path, regex=False, **kwargs):
+    def mock(self, method, path, regex=False, content_type="application/json", **kwargs):
         def decorator(func):
-            def inner(request, uri, response_headers):
-                remocker_request = RemockerRequest(
-                    request,
-                    uri,
-                    method,
-                    response_headers,
-                    uri_pattern=path if regex else None,
-                )
-                response = func(remocker_request)
+            def inner(request):
+                mock_request = RemockerRequest(request, pattern=path if regex else None)
+
+                response = func(mock_request)
                 self.storages.append(
-                    RemockerLog(request=remocker_request, response=response)
+                    RemockerLog(request=mock_request, response=response)
                 )
-                if response.headers is None:
-                    response.headers = response_headers
                 return response.to_tuple()
 
-            self._append_mocker(method, path, inner, regex=regex, **kwargs)
+            self._append_mocker(method, path, inner, regex=regex, content_type=content_type, **kwargs)
             return inner
 
         return decorator
 
-    def apply_mockers(self):
+    def apply_mockers(self, re_mock):
         for m in self.mockers:
-            httpretty.register_uri(**m)
+            re_mock.add_callback(**m)
         return True
 
     @contextmanager
-    def mocking(self, allow_net_connect=False, verbose=False):
-        with mocking(self, allow_net_connect=allow_net_connect, verbose=verbose) as app:
-            yield app
+    def mocking(self):
+        with responses.RequestsMock(assert_all_requests_are_fired=False) as r:
+            self.apply_mockers(r)
+            yield self
 
 
 @contextmanager
-def mocking(mocker_app: Remocker, allow_net_connect=False, verbose=False):
-    httpretty.reset()
-    httpretty.enable(allow_net_connect=allow_net_connect, verbose=verbose)
-    mocker_app.apply_mockers()
-    yield mocker_app
-    httpretty.disable()
-    httpretty.reset()
+def mocking(mocker_app: Remocker):
+    with mocker_app.mocking() as app:
+        yield app
 
 
 class RemockerResponse:
@@ -125,42 +106,26 @@ class RemockerRequest:
     def __init__(
             self,
             request,
-            uri,
-            method,
-            response_headers,
-            uri_pattern=None,
+            pattern=None,
     ):
+        self.method = request.method
+        self.url = request.url
+        self.path = request.path_url
+        self.params = request.params
         self.origin_request = request
         self.headers = request.headers
-        self.uri = uri
-        self.method = method
-        self.response_headers = response_headers
-        self.uri_pattern = uri_pattern
+        self.pattern = pattern
 
     @cached_property
     def url_params(self):
-        if self.uri_pattern is not None:
-            matched = re.search(self.uri_pattern, self.uri)
-            return matched.groupdict()
-        else:
+        if not self.pattern:
             return {}
+        matched = re.search(self.pattern, self.path)
+        return matched.groupdict()
 
     @cached_property
     def data(self):
         return _json.loads(self.origin_request.body)
-
-    @cached_property
-    def query_params(self):
-        result = {}
-        for key, value in self.origin_request.querystring.items():
-            if not value:
-                result[key] = None
-
-            if type(value) is list and len(value) == 1:
-                result[key] = value[0]
-            else:
-                result[key] = value
-        return result
 
 
 class RemockerLog:
--- tests.py
+++ tests.py
@@ -1,8 +1,9 @@
 from unittest import TestCase
 
 import requests
+import responses
 
-from remocker import Remocker, RemockerResponse, join_path, RemockerRequest
+from remocker import Remocker, RemockerResponse, join_path
 
 BASE_URL = 'https://test.com/base'
 mocker_app = Remocker(BASE_URL)
@@ -11,17 +12,17 @@ def _check_header(request):
     assert request.headers['x-test'] == 'test-header'
 
 @mocker_app.mock(method='GET', path='products')
-def get_products_mocker(request: RemockerRequest):
+def get_products_mocker(request):
     _check_header(request)
     response = RemockerResponse({
         'success': True,
-        'given_params': request.query_params,
+        'given_params': request.params,
     })
     return response
 
 
 @mocker_app.mock(method='POST', path='products')
-def create_product_mocker(request: RemockerRequest):
+def create_product_mocker(request):
     _check_header(request)
     response = RemockerResponse({
         'success': True,
@@ -31,7 +32,7 @@ def create_product_mocker(request: RemockerRequest):
 
 
 @mocker_app.mock(method='GET', path=r'products/(?P<product_id>\d+)', regex=True)
-def get_product_mocker(request: RemockerRequest):
+def get_product_mocker(request):
     _check_header(request)
     response = RemockerResponse({
         'success': True,
@@ -41,6 +42,20 @@ def get_product_mocker(request: RemockerRequest):
 
 
 class RemockerTestCase(TestCase):
+    def test_my_api(self):
+        with responses.RequestsMock() as rsps:
+            rsps.add(
+                responses.GET,
+                "http://twitter.com/api/1/foobar",
+                body="{}",
+                status=200,
+                content_type="application/json",
+            )
+            resp = requests.get("http://twitter.com/api/1/foobar")
+
+            assert resp.status_code == 200
+
+
     def test_mocking(self):
         headers = {
             'x-test': 'test-header'
